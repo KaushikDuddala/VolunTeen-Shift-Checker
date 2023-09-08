2a3b91a1e8b86876876744e276ee2c36d168f7c4
Kaushik Duddala <Kaushik.duddala.964@k12.friscoisd.org>
2023-09-08 14:37:37 +0000

    function to return number of shifts + cleanup

--- index.js
+++ index.js
@@ -1,33 +1,72 @@
-//nodejs fetch
 const fetch = require("node-fetch");
 const config = require("./config.json")
 
 async function getAuthToken(config){
-
-    let verificationTokenCookie, verificationTokenHTML, authToken;
-
-
+    
+    let verificationTokenCookie, verificationTokenHTML, authToken, invalidSessionTracker;
+    
+    
     await fetch("https://app.betterimpact.com/Login/LoginNoSearch").then(async res => {
         verificationTokenCookie = await res.headers.get('set-cookie').match(/__RequestVerificationToken=(.*?);/)[1]
         const resText = await res.text(); verificationTokenHTML = await resText.match(/"__RequestVerificationToken" type="hidden" value="(.*?)"/)[1];
     })
 
-    
+
     await fetch("https://app.betterimpact.com/Login/Login", {
         "headers": {
             "content-type": "application/x-www-form-urlencoded",
             "cookie": `__RequestVerificationToken=${verificationTokenCookie}`,
         },
-        "body": `__RequestVerificationToken=${verificationTokenHTML}&UserName=${config.username}&Password=${config.password}&ReturnUrl=&AgencyGuid=`,
-        "method": "POST",
-        "redirect": "manual"
+            "body": `__RequestVerificationToken=${verificationTokenHTML}&UserName=${config.username}&Password=${config.password}&ReturnUrl=&AgencyGuid=`,
+            "method": "POST",
+            "redirect": "manual"
     }).then(async res => {
         authToken = await res.headers.get('set-cookie').match(/.vol2auth=(.*?);/)[1]
     });
 
+
+    await fetch("https://app.betterimpact.com/Volunteer/Main", {
+        "headers": {
+            "cookie": `__RequestVerificationToken=${verificationTokenHTML}; .vol2auth=${authToken}; user_login_context=MIP`,
+        },
+        "body": null,
+        "method": "GET"
+    }).then(res => {
+        invalidSessionTracker = res.headers.get('set-cookie').match(/invalidSessionTracker=(.*?);/)[1]
+    });
     if (authToken){
-        return authToken
+        return {"authToken": authToken, "invalidSessionTracker": invalidSessionTracker}
     }else{
         return "error"
     }
 }
+
+async function getVolunteeringShifts(loginContext){
+    let shiftsAvailable = false;
+    let shiftsNumbers = [];
+    const vol2auth =  loginContext.authToken;
+    const invalidSessionTracker = loginContext.invalidSessionTracker;
+    const sessionHeartbeat = "OK";
+    
+    await fetch("https://app.betterimpact.com/Volunteer/Schedule/FilterOpportunities", {
+    "headers": {
+        "x-requested-with": "XMLHttpRequest",
+        "cookie": `invalidSessionTracker=${invalidSessionTracker}; .vol2auth=${vol2auth}; sessionHeartbeat=${sessionHeartbeat};`,
+    },
+    "body":"SortOrder=0&GroupByCategory=true&HasSingleMembership=True",
+    "method": "POST"
+    }).then(async res => {
+        const resText = await res.text();
+        await resText.match(/<td class="right">(\d+?)<\/td>/g).forEach(async item => {
+            shiftsNumbers.push(await item.match(/(\d+)/g))
+        })
+    });
+    if(shiftsNumbers.length > 0){
+        shiftsAvailable = true;
+    }
+    return {"shiftsAvailable": shiftsAvailable, "shiftsNumbers": shiftsNumbers}
+}
+async function main(){
+    console.log(await getVolunteeringShifts(await getAuthToken(config)))
+}
+main()
